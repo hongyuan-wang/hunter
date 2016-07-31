f4e853cb76e7f26425969bc7dbe1477593d22f27
yuanwhy <710136012@qq.com>
2016-07-31 22:27:18 +0800

    start jvm for class

--- main.go
+++ main.go
@@ -10,8 +10,14 @@ func main() {
 		fmt.Println("hunter [-options] class [args...]")
 	} else if cmd.version {
 		fmt.Println("hunter 0.0.1")
+	} else if cmd.class != "" {
+		startJvm(cmd)
 	} else {
 		fmt.Println("hunter -help")
 	}
 
+}
+
+func startJvm(cmd *Cmd) {
+	fmt.Printf("start jvm hunter to run %s ...\n", cmd.class)
 }
